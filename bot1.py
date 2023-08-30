e8cd97913e79d9d24bd3feacbef00b8b10fe5a48
Faheem Shehzad <faheemshehzad.te@gmail.com>
2023-08-31 01:52:53 +0500

    changed browser

--- bot1.py
+++ bot1.py
@@ -1,71 +1,12 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromiumService
-from webdriver_manager.chrome import ChromeDriverManager
-import pickle
-import time
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-import sys
-from datetime import datetime, timedelta
-import traceback
-import platform
-# import undetected_chromedriver as uc
 
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.chrome.options import Options
+from get_browser import get_browser
+
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 import csv
 import os
-os_name = platform.system()
-
-options = webdriver.ChromeOptions()
-if os_name == "Windows":
-    print("Running for Windows...")
-    chrome_path = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
-    user_data_dir = r"C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\User Data\\"
-    options.add_argument(f"--user-data-dir={user_data_dir}")
-
-    # provide the profile name with which we want to open browser
-    options.add_argument(r'--profile-directory=Default')
-    # from Screenshot import Screenshot
-
-else:
-    print("Running For Mac...")
-    # provide location where chrome stores profiles
-    user_data_dir = f"/Users/techesthete0001/Library/Application Support/Google/Chrome"
-    chrome_path = "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-    options.add_argument(f"--user-data-dir={user_data_dir}")
-
-    # provide the profile name with which we want to open browser
-    options.add_argument(r'--profile-directory=Profile 1')
-
-# disable the AutomationControlled feature of Blink rendering engine
-options.add_argument('--disable-blink-features=AutomationControlled')
-
-# disable pop-up blocking
-options.add_argument('--disable-popup-blocking')
-
-# start the browser window in maximized mode
-options.add_argument('--start-maximized')
-
-# disable extensions
-options.add_argument('--disable-extensions')
-
-# disable sandbox mode
-options.add_argument('--no-sandbox')
-
-# disable shared memory usage
-options.add_argument('--disable-dev-shm-usage')
-
-service = Service(chrome_path)  # Replace with the path to your Chrome WebDriver executable
-
-browser = webdriver.Chrome(options=options)
 
 # Define constants and file paths
 SEARCH_TERM = "fleuriste"
@@ -75,22 +16,26 @@ SCRIPT_DIRECTORY = os.path.dirname(os.path.abspath(__file__))
 CSV_FILE_PATH = os.path.join(SCRIPT_DIRECTORY, DATA_FILE)
 CITIES_FILE_PATH = os.path.join(SCRIPT_DIRECTORY, CITIES_FILE)
 
-import undetected_chromedriver as uc
-import getpass
-username = getpass.getuser()
-if os.name == 'nt':  # For Windows
-    chrome_path = f"C:/Users/{username}/AppData/Local/Google/Chrome/User Data/profile 1"
-options = uc.ChromeOptions()
-options.headless = False
-def get_hector_profile_directory():
-    # Replace 'C:\\Users\\th_im\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 4'
-    # with the actual path to the "Hector" profile directory.
-    return r'C:\\Users\\th_im\\Downloads\\Facebook Page Details Scrapper\\data\\Default'
+# import undetected_chromedriver as uc
+# import getpass
+# username = getpass.getuser()
+# if os.name == 'nt':  # For Windows
+#     chrome_path = f"C:/Users/{username}/AppData/Local/Google/Chrome/User Data/profile 1"
+# options = uc.ChromeOptions()
+# options.headless = False
+# def get_hector_profile_directory():
+#     # Replace 'C:\\Users\\th_im\\AppData\\Local\\Google\\Chrome\\User Data\\Profile 4'
+#     # with the actual path to the "Hector" profile directory.
+#     return r'C:\\Users\\th_im\\Downloads\\Facebook Page Details Scrapper\\data\\Default'
+#
+# # Get the path to the "Hector" profile directory
+# hector_profile_path = get_hector_profile_directory()
+# options.add_argument(f"user-data-dir={hector_profile_path}")
+# driver = uc.Chrome(use_subprocess=True, options=options)
 
-# Get the path to the "Hector" profile directory
-hector_profile_path = get_hector_profile_directory()
-options.add_argument(f"user-data-dir={hector_profile_path}")
-driver = uc.Chrome(use_subprocess=True, options=options)
+driver = get_browser()
+
+
 # Function to scroll down to the end of search results
 def scroll_to_end():
     while True:
---
+++ get_browser.py
@@ -0,0 +1,51 @@
+import platform
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+import getpass
+
+os_name = platform.system()
+
+options = webdriver.ChromeOptions()
+
+
+username = getpass.getuser()
+
+
+def get_browser():
+    # if os_name == "nt":
+    # print("Running for Windows...")
+
+    # if os.name == 'nt':  # For Windows
+    chrome_path = f"C:/Users/{username}/AppData/Local/Google/Chrome/User Data/profile 1"
+
+    # chrome_path = r"C:\Program Files\Google\Chrome\Application\chrome.exe"
+    user_data_dir = r'C:\\Users\\th_im\\Downloads\\Facebook Page Details Scrapper\\data\\Default'
+    # user_data_dir = r"C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\User Data\\"
+    options.add_argument(f"--user-data-dir={user_data_dir}")
+
+    # provide the profile name with which we want to open browser
+    # options.add_argument(r'--profile-directory=Default')
+    # from Screenshot import Screenshot
+
+    # disable the AutomationControlled feature of Blink rendering engine
+    options.add_argument('--disable-blink-features=AutomationControlled')
+
+    # disable pop-up blocking
+    options.add_argument('--disable-popup-blocking')
+
+    # start the browser window in maximized mode
+    options.add_argument('--start-maximized')
+
+    # disable extensions
+    options.add_argument('--disable-extensions')
+
+    # disable sandbox mode
+    options.add_argument('--no-sandbox')
+
+    # disable shared memory usage
+    options.add_argument('--disable-dev-shm-usage')
+
+    service = Service(chrome_path)  # Replace with the path to your Chrome WebDriver executable
+
+    browser = webdriver.Chrome(options=options)
+    return browser
